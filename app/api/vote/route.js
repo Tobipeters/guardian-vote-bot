95bbd9ec7a955f6ea2ce827c553040185b093a0c
Tobi Peter <oluwatobilobapeterschem@gmail.com>
2025-04-07 11:37:56 +0100

    Trying to puppeteer issue

--- app/api/vote/route.js
+++ app/api/vote/route.js
@@ -1,30 +1,29 @@
-
-import puppeteer from 'puppeteer'
+import puppeteer from "puppeteer";
 // import chromium from 'chrome-aws-lambda'
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 // fetch a random user
-async function fetchRandomEmail () {
-  const response = await fetch('https://randomuser.me/api/')
-  const data = await response.json()
+async function fetchRandomEmail() {
+  const response = await fetch("https://randomuser.me/api/");
+  const data = await response.json();
 
-  const user = data.results[0]
-  const email = user.email
+  const user = data.results[0];
+  const email = user.email;
 
   // Replace @example with @gmail.com
-  const username = email.split('@')[0]
-  return `${username}@gmail.com`
+  const username = email.split("@")[0];
+  return `${username}@gmail.com`;
 }
 
-export async function POST (request) {
-  let browser
-  let page
+export async function POST(request) {
+  let browser;
+  let page;
 
-  const randomEmail = await fetchRandomEmail()
+  const randomEmail = await fetchRandomEmail();
 
   try {
     // This runs the browser process in the background
-    const browser = await puppeteer.launch({ headless: true })
+    browser = await puppeteer.launch({ headless: true });
 
     // use this if you want the browser to keep opening so you can see the voting in action
     // browser = await chromium.puppeteer.launch({
@@ -34,39 +33,39 @@ export async function POST (request) {
     //   headless: chromium.headless
     // })
 
-    page = await browser.newPage()
+    page = await browser.newPage();
 
     await page.goto(
-      'https://riceuniversity.co1.qualtrics.com/jfe/form/SV_bIq5iYJz8sws2yi',
-      { waitUntil: 'networkidle2', timeout: 6000000 }
-    )
+      "https://riceuniversity.co1.qualtrics.com/jfe/form/SV_bIq5iYJz8sws2yi",
+      { waitUntil: "networkidle2", timeout: 6000000 }
+    );
 
     // select the necessary elements
-    const emailSelector = 'input[type="text"]'
-    await page.waitForSelector(emailSelector)
-    await page.type(emailSelector, randomEmail)
+    const emailSelector = 'input[type="text"]';
+    await page.waitForSelector(emailSelector);
+    await page.type(emailSelector, randomEmail);
 
-    const participantSelector = `[aria-labelledby="choice-display-QID2-19"]`
-    await page.click(participantSelector)
+    const participantSelector = `[aria-labelledby="choice-display-QID2-19"]`;
+    await page.click(participantSelector);
 
-    const buttonSelector = 'button'
-    await page.waitForSelector(buttonSelector)
-    await page.click(buttonSelector)
+    const buttonSelector = "button";
+    await page.waitForSelector(buttonSelector);
+    await page.click(buttonSelector);
 
     // await page.close() // ✅ close the tab
     // await browser.close()
 
     return NextResponse.json({
       success: true,
-      message: `Vote for The GUARDIAN submitted successfully with email ${randomEmail}`
-    })
+      message: `Vote for The GUARDIAN submitted successfully with email ${randomEmail}`,
+    });
   } catch (error) {
-    if (page) await page.close().catch(() => {})
-    if (browser) await browser.close().catch(() => {})
+    if (page) await page.close().catch(() => {});
+    if (browser) await browser.close().catch(() => {});
 
     return NextResponse.json({
       success: false,
-      message: `Failed to submit vote. Error: ${error.message}`
-    })
+      message: `Failed to submit vote. Error: ${error.message}`,
+    });
   }
 }
